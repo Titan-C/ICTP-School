92581fe37512c9ef66e76d2c3018bb172ef61483
Oscar Najera <najera.oscar@gmail.com>
2012-02-21 19:13:11 +0100

    First version completed solution

--- python/ExcerciseSessionThu21/main.py
+++ python/ExcerciseSessionThu21/main.py
@@ -1,11 +1,34 @@
+from os import listdir
+from sys import argv
 
-import os
-
-def main():
+def main(dir):
+	files = listFiles(dir)
+	counter=0
+	for file in files:
+		if testLine(dir+file,4):
+			counter +=fixLine(dir+file,4)
+	print counter, "files edited"
 
 def listFiles(dir):
-    return os.listdir(dir)
+    return listdir(dir)
 
-def testLine():
+def testLine(file,line):
+    f = open(file,'r')
+    text=f.readlines()
+    f.close()
+    if text[line] == 'Sex: N\n':
+        return True
+    return False
 
-def fixLine():
+def fixLine(file,line):
+    f=open(file)
+    text=f.readlines()
+    f.close()
+    text[line] = 'Sex: M\n'
+    g=open(file,'w')
+    for inf in text:
+        g.write(inf)
+    g.close()
+    return 1
+
+main(argv[1])
