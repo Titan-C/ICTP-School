9988b89037c74b8fe2d6f6c6d1e6da3de723675f
Oscar Najera <najera.oscar@gmail.com>
2012-02-21 17:12:44 +0100

    Completed Prime number test script

--- python/primenumbers/findPrimes.py
+++ python/primenumbers/findPrimes.py
@@ -3,7 +3,27 @@ List whether the numbers passed in the command line are prime.
 '''
 
 from sys import argv
+from math import sqrt
+
+def isPrime(n):
+	'''Returns True iff n is prime'''
+	if n<=1:
+		return False
+
+	if n<=3:
+		return True 
+
+	if n%2 == 0: return False
+
+	cntr = 3
+	while cntr <=sqrt(n):
+		if n%cntr == 0:
+			return False
+		cntr += 2
+	return True
 
 for i in range(1,len(argv)):
-	print argv[i]
+	primeTest = isPrime(int(argv[i]))
+	if primeTest: print argv[i],"is prime"
+	else: print argv[i],"is not prime"
 
