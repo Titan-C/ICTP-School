91a395d93ada1188fef6934112b21511b00896b4
Oscar Najera <najera.oscar@gmail.com>
2013-03-13 16:42:57 +0100

    C version of great circle

---
+++ python/Cython/greatCircle.c
@@ -0,0 +1,24 @@
+#include <math.h>
+#include <stdio.h>
+#define NUM 500000
+
+float great_circle(float lon1, float lat1, float lon2, float lat2){
+    float radius = 3956.0;
+    float pi = 3.14159265;
+    float x = pi/180.0;
+    float a,b,theta,c;
+
+    a = (90.0-lat1)*(x);
+    b = (90.0-lat2)*(x);
+    theta = (lon2-lon1)*(x);
+    c = acosf((cosf(a)*cosf(b)) + (sinf(a)*sinf(b)*cosf(theta)));
+    return radius*c;
+}
+
+int main() {
+    int i;
+    float x;
+    for (i=0; i <= NUM; i++) 
+        x = great_circle(-72.345, 34.323, -61.823, 54.826);
+    printf("%f", x);
+}
