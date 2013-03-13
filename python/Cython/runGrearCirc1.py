be6a5b4e071ea9a399bca58aa581871cff45568d
Oscar Najera <najera.oscar@gmail.com>
2013-03-13 15:10:28 +0100

    Add new Cython code to great Circle rutine

--- python/Cython/runGrearCirc1.py
+++ python/Cython/runGrearCirc1.py
@@ -6,3 +6,7 @@ num = 500000
 t = timeit.Timer("p1.great_circle(%f,%f,%f,%f)" % (lon1,lat1,lon2,lat2), 
                        "import greatCirc1 as p1")
 print "Pure python function", t.timeit(num), "sec"
+
+t = timeit.Timer("c1.great_circle(%f,%f,%f,%f)" % (lon1,lat1,lon2,lat2), 
+                       "import greatCirc2 as c1")
+print "Cython function with python math", t.timeit(num), "sec"
