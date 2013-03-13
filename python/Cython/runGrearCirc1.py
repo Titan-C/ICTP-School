9b78d6b9de096602fcc7382ec9ea99bd04a08504
Oscar Najera <najera.oscar@gmail.com>
2013-03-13 15:18:44 +0100

    Use lib math.h in code

--- python/Cython/runGrearCirc1.py
+++ python/Cython/runGrearCirc1.py
@@ -7,6 +7,10 @@ t = timeit.Timer("p1.great_circle(%f,%f,%f,%f)" % (lon1,lat1,lon2,lat2),
                        "import greatCirc1 as p1")
 print "Pure python function", t.timeit(num), "sec"
 
-t = timeit.Timer("c1.great_circle(%f,%f,%f,%f)" % (lon1,lat1,lon2,lat2), 
-                       "import greatCirc2 as c1")
+t = timeit.Timer("c2.great_circle(%f,%f,%f,%f)" % (lon1,lat1,lon2,lat2), 
+                       "import greatCirc2 as c2")
 print "Cython function with python math", t.timeit(num), "sec"
+
+t = timeit.Timer("c3.great_circle(%f,%f,%f,%f)" % (lon1,lat1,lon2,lat2), 
+                       "import greatCirc3 as c3")
+print "Cython function with c math", t.timeit(num), "sec"
