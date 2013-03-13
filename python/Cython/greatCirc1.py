1aa2b38e66eb28520913535a8ab10bcda2eb3589
Oscar Najera <najera.oscar@gmail.com>
2013-03-13 14:50:39 +0100

    Great Circle rutine

---
+++ python/Cython/greatCirc1.py
@@ -0,0 +1,24 @@
+import math
+
+def great_circle(lon1,lat1,lon2,lat2):
+    radius = 3956 #miles
+    x = math.pi/180.0
+
+    a = (90.0-lat1)*(x)
+    b = (90.0-lat2)*(x)
+    theta = (lon2-lon1)*(x)
+    c = math.acos((math.cos(a)*math.cos(b)) +
+                  (math.sin(a)*math.sin(b)*math.cos(theta)))
+    return radius*c
+
+def main():
+  import timeit
+
+  lon1, lat1, lon2, lat2 = -72.345, 34.323, -61.823, 54.826
+  num = 500000
+
+  t = timeit.Timer("great_circle(%f,%f,%f,%f)"%(lon1,lat1,lon2,lat2),"from __main__ import great_circle")
+  print "Pure python function", t.timeit(num), "sec"
+
+if __name__ == "__main__":
+  main()
